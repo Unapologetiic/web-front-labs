b13ec654fb93765e79e82c3ee7c8b63d4eebdda8
Unapologetiic <kudearovaana@gmail.com>
2025-05-06 21:19:28 +0700

    книги

---
+++ lab6/books.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: 'Лао-цзы',
+        name: 'Дао дэ цзин. Канон о Дао и дэ',
+        publicationYear: 2022,
+        price: 550,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Карл Саган',
+        name: 'Космос',
+        publicationYear: 2024,
+        price: 870,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Баладжи Касал',
+        name: 'Ошибки разумного инвестора',
+        publicationYear: 2025,
+        price: 780,
+        bestseller: false,
+        available: false
+    },
+    {
+        author: 'Юваль Ной Харари',
+        name: 'Sapiens',
+        publicationYear: 2011,
+        price: 890,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Стивен Хокинг',
+        name: 'Краткая история времени',
+        publicationYear: 1988,
+        price: 640,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Нассим Талеб',
+        name: 'Черный лебедь',
+        publicationYear: 2007,
+        price: 720,
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Даниел Канеман',
+        name: 'Думай медленно... решай быстро',
+        publicationYear: 2011,
+        price: 810,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Ричард Фейнман',
+        name: 'Вы, конечно, шутите, мистер Фейнман!',
+        publicationYear: 1985,
+        price: 670,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Джордж Оруэлл',
+        name: '1984',
+        publicationYear: 1949,
+        price: 460,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Михаил Булгаков',
+        name: 'Мастер и Маргарита',
+        publicationYear: 1967,
+        price: 580,
+        bestseller: true,
+        available: false
+    }
+];
